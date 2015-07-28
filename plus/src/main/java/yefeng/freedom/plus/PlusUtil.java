58ff3837e4a634dc991a71c39e60e99524edc1fc
feng ye <yefeng.freedom@gmail.com>
2015-07-28 18:41:37 +0800

    init

---
+++ plus/src/main/java/yefeng/freedom/plus/PlusUtil.java
@@ -0,0 +1,13 @@
+package yefeng.freedom.plus;
+
+/**
+ * Created by yefeng on 7/28/15.
+ * github:yefengfreedom
+ */
+public class PlusUtil {
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+}
